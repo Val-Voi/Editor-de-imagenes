1982b0628197ce3e9ea38c139576de4116d38d31
Voifar <francisco.larad@sansano.usm.cl>
2016-08-05 11:34:46 -0400

    Add files via upload

--- Editor de Fotos.py
+++ Editor de Fotos.py
@@ -1,8 +1,9 @@
 from PIL import Image
 import numpy
 
+
 #Escala de Grises : Promedio
-#Negativo : 
+#Negativo : 255- color
 
 def convertirImagenAArchivo(nombreImagen, nombreDestino):
     imagen = Image.open(nombreImagen)
@@ -39,6 +40,145 @@ def convertirMatrizAImagen(matriz, nombreSalida):
     im.save(nombreSalida)
     return True
 
-convertirImagenAArchivo('Seesmic_Logo.png', 'matriz.txt')
+
+def espejo_vertical():
+    convertirImagenAArchivo("Seesmic_Logo.png","matriz.txt")
+    matriz = leerArchivo("matriz.txt")
+    for linea in matriz:
+        linea.reverse()
+    convertirMatrizAImagen(matriz, "reflejo-v.png")
+
+#espejo_vertical()
+
+
+
+def BlackAndWhite():
+    convertirImagenAArchivo("Seesmic_Logo.png","matriz.txt")
+    matriz = leerArchivo("matriz.txt")
+    
+    cont= 0
+    for linea in matriz:
+        cont2 = 0
+        for pixel in linea: #[R,G,B]
+            R,G,B = pixel
+            prom = int((R+G+B)/3)
+            pixel2 = [prom,prom,prom]
+            matriz[cont][cont2] = pixel2
+            cont2 +=1
+        cont+=1
+    convertirMatrizAImagen(matriz, "blackandwhite.png")
+
+#BlackAndWhite()
+
+def Negativo():
+    convertirImagenAArchivo("Seesmic_Logo.png","matriz.txt")
+    matriz = leerArchivo("matriz.txt")
+    
+    cont= 0
+    for linea in matriz:
+        cont2 = 0
+        for pixel in linea: #[R,G,B]
+            R,G,B = pixel
+            RN = 255-R
+            GN = 255-G
+            BN = 255-B
+            pixel2 = [RN,GN,BN]
+            matriz[cont][cont2] = pixel2
+            cont2 +=1
+        cont+=1
+    convertirMatrizAImagen(matriz, "negativo.png")
+
+#Negativo()
+
+
+def Glitch():
+    convertirImagenAArchivo("Seesmic_Logo.png","matriz.txt")
+    matriz = leerArchivo("matriz.txt")
+
+    vertical = len(matriz)
+    arriba = 0 + int(vertical/3)
+    abajo = vertical - int(vertical/3)
+    rango = (arriba,abajo)
+    mov = int((len(matriz[0])/4))
+    cont = 0
+    cont2 = 0
+    for linea in matriz:
+        if cont < rango[1] and cont > rango[0]:
+            while cont2 < mov :
+                for pixel in linea:
+                    matriz[cont].append(pixel)
+                    matriz[cont].remove(pixel)
+                    cont2 +=1
+            cont2 = 0
+        cont +=1
+    convertirMatrizAImagen(matriz, "glitch.png")
+    
+            
+            
+
+
+
+Glitch()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
